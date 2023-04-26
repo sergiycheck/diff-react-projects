2c823e76aedc9053c93f75daeadb6d74a49ea318
serhii <sergiycheck@gmail.com>
2023-04-26 11:01:17 +0300

    add webpack with daisy ui

---
+++ dayisy-ui/examples/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.css";
+
+const App = (props) => {
+  return (
+    <div>
+      <button className="btn">Hello daisyUI</button>
+      <p>test 3 1 3 4</p>
+    </div>
+  );
+};
+export default App;
